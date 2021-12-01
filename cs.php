0b0b45ed0056a643cf888f3f45af58aa89aee51f
jagoiklan <84367574+jagoiklan@users.noreply.github.com>
2021-12-01 18:27:32 +0700

    Update cs.php

--- cs.php
+++ cs.php
@@ -3,6 +3,6 @@
 $nomor_tiara = array("6285624440729");
 $nomor_seva = array("6285723464839"); 
 $nohp = $nomor_seva[array_rand($nomor_seva)]; 
-$text = "Hi kak saya mau konsultasi atau beli *GOAMI GLUTASKIN* nya, Ready ?";
+$text = "Holla kak saya mau konsultasi atau beli *GOAMI GLUTASKIN* nya, Ready ?";
 header("Location: https://api.whatsapp.com/send?phone=$nohp&text=$text"); ?>"">
 </script>
